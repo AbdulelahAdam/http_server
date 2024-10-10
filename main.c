e262a1b3fc63c0aa7b37b4f1ca2071a801c31cf3
Abdulelah Adam <abdulelah859@gmail.com>
2024-10-10 23:48:45 +0300

    HTTP Method parse is complete

--- main.c
+++ main.c
@@ -54,13 +54,15 @@ if((server_port = bind(status, servinfo->ai_addr, servinfo->ai_addrlen)) == -1)
 printf("Port binding success. Listening at port 8080.....\n");
 // listen at port for incoming requests:
 size_t accepted;
-char* buff = (char*)malloc(4096);  
 size_t sock_len = 25;
+char* buff_received = (char*)malloc(4096);
+char* buff_sent = (char*)malloc(4096);
 char* local_addr = "localhost";
-char* response_header = (char*) malloc(4096);
+char* response_header = (char*) malloc(256);
 FILE *fptr;
 size_t file_size;
-ssize_t bytes_received, sent_bytes;
+size_t bytes_received;
+char method[7];
 listen(status, 2);
 while(1)
 {
@@ -71,29 +73,45 @@ while(1)
         printf("Connected to Server successfully.\n");
 
       // receive client request here
-        //...... 
       //Send response here
       while(1) // response
-      {
-        fptr = fopen("./static/index.html", "r");
-        bytes_received = getdelim(&buff, &file_size, '\0', fptr);
-        sprintf(response_header, "%s %ld %s","HTTP/1.0 200 OK\r\nContent-Type: text/html; charset=utf-8\r\nContent-Length: ", bytes_received, "\r\n\r\n");
-        send(accepted, response_header, strlen(response_header), 0);
+      { 
+        bytes_received = recv(accepted, buff_received, 4096, 0);
+        if(bytes_received > 0)
+        {
+          for(int i = 0; i < 7; i++)
+          {
+            if(buff_received[i] == ' ')
+            {
+              method[i] = '\0';
+              break;
+            }
 
+            method[i] = (char)buff_received[i];
+
+          }
+          //method[7] = '\0';
+          buff_sent = "<h1 style='text-align: center; color: red;'>This page is under construction</h1>\n";
+          file_size = strlen(buff_sent);
+          sprintf(response_header, "%s %ld %s","HTTP/1.0 200 OK\r\nContent-Type: text/html; charset=utf-8\r\nContent-Length: ", bytes_received, "\r\n\r\n");
+        printf("method: %s\n", method) ;
+        send(accepted, response_header, strlen(response_header), 0);
           
-        if (send(accepted, buff, bytes_received, 0) == -1) {
+        if (send(accepted, buff_sent, file_size, 0) == -1) 
+          {
                perror("Error in sending file.");
                exit(1);
+          }
         }
-
-        fclose(fptr);
+        
        //printf("%s", notes);  // Print each line read
 
          // bzero(buff, sizof());
           //printf("sent: %ld\n", sent_bytes);
           //bytes_received = 0;
 //          if()
-          break;
+        bzero(method, 7);
+         break;
         
 
       }
@@ -144,7 +162,8 @@ while(1)
 */
 fclose(fptr);
 free(local_addr);
-free(buff);
+free(buff_sent);
+free(buff_received);
 free(response_header);
 
 freeaddrinfo(servinfo); // free the linked-list
